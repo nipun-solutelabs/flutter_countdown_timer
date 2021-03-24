391e9f1bd4670761da725889e4386586a177f58c
nipun <nipun.s@solutelabs.com>
2021-03-24 18:29:46 +0530

    init commit

---
+++ example/lib/countdown_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/index.dart';
+
+class CountdownPage extends StatefulWidget {
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<CountdownPage> {
+  CountdownController countdownController =
+      CountdownController(duration: Duration(minutes: 1));
+  bool isRunning = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Countdown(
+            countdownController: countdownController,
+            builder: (_, Duration time) {
+              return Text(
+                '${time?.inSeconds ?? 0}',
+                style: TextStyle(fontSize: 80),
+              );
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child:
+            Icon(isRunning ? Icons.stop : Icons.play_arrow),
+        onPressed: () {
+          if (!countdownController.isRunning) {
+            countdownController.start();
+            setState(() {
+              isRunning = true;
+            });
+          } else {
+            countdownController.stop();
+            setState(() {
+              isRunning = false;
+            });
+          }
+        },
+      ),
+    );
+  }
+}
